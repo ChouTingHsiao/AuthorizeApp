5caed033d94e9d5fa33101ca3be514d8cb235d50
Chou-ting-hsiao <kk156868@gmail.com>
2021-03-06 16:10:57 +0000

    add SwPush

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,7 +7,7 @@ import { Store } from '@ngrx/store';
 import { TableEnum } from '@shared/Enum/table.enum';
 import { Read } from '@shared/Ngrx/Actions/maintain.action';
 import { entityToArray } from '@shared/Method/object.method';
-
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-main',
@@ -21,8 +21,19 @@ export class MainComponent implements OnInit {
   menus: Observable<Menu[]>;
 
   constructor(private router: Router,
+              private swPush: SwPush,
               private store: Store<any>,
-              private menuService: MenuService) { }
+              private menuService: MenuService) {
+
+    this.swPush.notificationClicks.subscribe(({action, notification}) => {
+
+      console.log(action);
+
+      console.log(notification);
+
+    });
+
+  }
 
   ngOnInit() {
 
