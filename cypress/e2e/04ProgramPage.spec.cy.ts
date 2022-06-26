63a7304cea3d4799f074281b1bd7fa5f97c0ceec
T178257-周庭孝 <stephen.chou@kerrytj.com>
2022-06-26 16:17:22 +0800

    ProgramPage add detail test

--- cypress/e2e/04ProgramPage.spec.cy.ts
+++ cypress/e2e/04ProgramPage.spec.cy.ts
@@ -53,6 +53,54 @@ describe('My ProgramPage Test', () => {
     cy.CheckColumnValue('name', newName)
   })
 
+  const detailElement = 'app-table tbody tr.detail-row';
+
+  it('Should Display Detail', () => {
+
+    cy.ClickButtonByColumn('button[aria-label="Detail"]', 'name', 'TEST2')
+
+    cy.get(`${detailElement} button#btnAdd`).should("be.visible")
+  })
+
+  it('Should Add Detail', () => {
+
+    cy.get(`${detailElement} button#btnAdd`).click()
+
+    cy.get('input[data-placeholder="name"]').type('TEST1')
+
+    cy.get('input[data-placeholder="remark"]').type('TEST1')
+
+    cy.cacheElement('previousDetailTotal', `${detailElement} div.mat-paginator-range-label`)
+
+    cy.get('button.mat-button.mat-button-base').eq(0).click()
+
+    cy.wait(500)
+
+    cy.cacheElement('currentDetailTotal', `${detailElement} div.mat-paginator-range-label`)
+
+    cy.CheckTotalCount('previousDetailTotal', 'currentDetailTotal')
+  })
+
+  it('Should Edit Detail', () => {
+
+    cy.ClickButtonByColumn(`button#btnEdit`, 'name', 'TEST1', true)
+
+    const newName = 'TEST2'
+
+    cy.get('input[data-placeholder="name"]').clear().type(newName)
+
+    cy.get('button.mat-button.mat-button-base').eq(0).click()
+
+    cy.wait(500)
+
+    cy.CheckColumnValue('name', newName, true)
+  })
+
+  it('Should Delete Detail', () => {
+    
+    cy.ClickButtonByColumn('button#btnDelete', 'name', 'TEST2', true)
+  })
+
   it('Should Delete Item', () => {
     
     cy.ClickButtonByColumn('button#btnDelete', 'name', 'TEST2')
